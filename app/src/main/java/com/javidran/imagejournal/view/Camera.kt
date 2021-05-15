86e0ba2397ae3b16c34bf599364e70f322c0f2b9
Joan <joan.gamez@est.fib.upc.edu>
2021-05-15 16:15:12 +0200

    camera furulating

--- app/src/main/java/com/javidran/imagejournal/view/Camera.kt
+++ app/src/main/java/com/javidran/imagejournal/view/Camera.kt
@@ -3,10 +3,13 @@ package com.javidran.imagejournal.view
 import android.Manifest
 import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.camera.core.*
+import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
@@ -30,6 +33,7 @@ class Camera : Fragment() {
     private var imageCapture: ImageCapture? = null
 
     private var _binding: FragmentCameraBinding? = null
+
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
@@ -57,7 +61,8 @@ class Camera : Fragment() {
         } else {
             activity?.let {
                 ActivityCompat.requestPermissions(
-                    it, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
+                    it, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS
+                )
             }
         }
 
@@ -71,33 +76,83 @@ class Camera : Fragment() {
         return view
     }
 
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<String>, grantResults:
+        IntArray
+    ) {
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionsGranted()) {
+                startCamera()
+            } else {
+                Toast.makeText(
+                    context,
+                    "Permissions not granted by the user.",
+                    Toast.LENGTH_SHORT
+                ).show()
+                activity?.finish()
+            }
+        }
+    }
+
     private fun takePhoto() {}
 
-    private fun startCamera() {}
+    private fun startCamera() {
+        val cameraProviderFuture = context?.let { ProcessCameraProvider.getInstance(it) }
 
-    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
-        context?.let { it1 ->
-            ContextCompat.checkSelfPermission(
-                it1, it)
-        } == PackageManager.PERMISSION_GRANTED
+        cameraProviderFuture?.addListener({
+            // Used to bind the lifecycle of cameras to the lifecycle owner
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+
+            // Preview
+            val preview = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(binding.preview.surfaceProvider)
+                }
+
+            // Select front camera as a default (granizado selfie intensifies!)
+            val cameraSelector = CameraSelector.DEFAULT_FRONT_CAMERA
+
+            try {
+                // Unbind use cases before rebinding
+                cameraProvider.unbindAll()
+
+                // Bind use cases to camera
+                cameraProvider.bindToLifecycle(
+                    this, cameraSelector, preview
+                )
+
+            } catch (exc: Exception) {
+                Log.e(TAG, "Use case binding failed", exc)
+            }
+        }, ContextCompat.getMainExecutor(context))
     }
 
-    private fun getOutputDirectory(): File {
-        val mediaDir = activity?.externalMediaDirs?.firstOrNull()?.let {
-            File(it, resources.getString(R.string.app_name)).apply { mkdirs() } }
-        return if (mediaDir != null && mediaDir.exists())
-            mediaDir else context?.filesDir!!
-    }
+private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+    context?.let { it1 ->
+        ContextCompat.checkSelfPermission(
+            it1, it
+        )
+    } == PackageManager.PERMISSION_GRANTED
+}
 
-    override fun onDestroy() {
-        super.onDestroy()
-        cameraExecutor.shutdown()
+private fun getOutputDirectory(): File {
+    val mediaDir = activity?.externalMediaDirs?.firstOrNull()?.let {
+        File(it, resources.getString(R.string.app_name)).apply { mkdirs() }
     }
+    return if (mediaDir != null && mediaDir.exists())
+        mediaDir else context?.filesDir!!
+}
 
-    companion object {
-        private const val TAG = "CameraXBasic"
-        private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
-        private const val REQUEST_CODE_PERMISSIONS = 10
-        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
-    }
+override fun onDestroy() {
+    super.onDestroy()
+    cameraExecutor.shutdown()
+}
+
+companion object {
+    private const val TAG = "CameraXBasic"
+    private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
+    private const val REQUEST_CODE_PERMISSIONS = 10
+    private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+}
 }
