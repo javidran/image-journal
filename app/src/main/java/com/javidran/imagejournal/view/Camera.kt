0981114951184758df97d8c7c247d3e8b0dc8e4d
Joan <joan.gamez@est.fib.upc.edu>
2021-05-15 16:35:51 +0200

    images are saved!

--- app/src/main/java/com/javidran/imagejournal/view/Camera.kt
+++ app/src/main/java/com/javidran/imagejournal/view/Camera.kt
@@ -2,6 +2,7 @@ package com.javidran.imagejournal.view
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -18,6 +19,7 @@ import com.javidran.imagejournal.databinding.FragmentCameraBinding
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.fragment_camera.*
 import java.io.File
+import java.text.SimpleDateFormat
 import java.util.*
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
@@ -94,7 +96,39 @@ class Camera : Fragment() {
         }
     }
 
-    private fun takePhoto() {}
+    private fun takePhoto() {
+        // Get a stable reference of the modifiable image capture use case
+        val imageCapture = imageCapture ?: return
+
+        // Create time-stamped output file to hold the image
+        val photoFile = File(
+            outputDirectory,
+            SimpleDateFormat(
+                FILENAME_FORMAT, Locale.US
+            ).format(System.currentTimeMillis()) + ".jpg"
+        )
+
+        // Create output options object which contains file + metadata
+        val outputOptions = ImageCapture.OutputFileOptions.Builder(photoFile).build()
+
+        // Set up image capture listener, which is triggered after photo has
+        // been taken
+        imageCapture.takePicture(
+            outputOptions,
+            ContextCompat.getMainExecutor(context),
+            object : ImageCapture.OnImageSavedCallback {
+                override fun onError(exc: ImageCaptureException) {
+                    Log.e(TAG, "Photo capture failed: ${exc.message}", exc)
+                }
+
+                override fun onImageSaved(output: ImageCapture.OutputFileResults) {
+                    val savedUri = Uri.fromFile(photoFile)
+                    val msg = "Photo capture succeeded: $savedUri"
+                    Toast.makeText(context, msg, Toast.LENGTH_SHORT).show()
+                    Log.d(TAG, msg)
+                }
+            })
+    }
 
     private fun startCamera() {
         val cameraProviderFuture = context?.let { ProcessCameraProvider.getInstance(it) }
@@ -110,6 +144,9 @@ class Camera : Fragment() {
                     it.setSurfaceProvider(binding.preview.surfaceProvider)
                 }
 
+            imageCapture = ImageCapture.Builder()
+                .build()
+
             // Select front camera as a default (granizado selfie intensifies!)
             val cameraSelector = CameraSelector.DEFAULT_FRONT_CAMERA
 
@@ -119,8 +156,7 @@ class Camera : Fragment() {
 
                 // Bind use cases to camera
                 cameraProvider.bindToLifecycle(
-                    this, cameraSelector, preview
-                )
+                    this, cameraSelector, preview, imageCapture)
 
             } catch (exc: Exception) {
                 Log.e(TAG, "Use case binding failed", exc)
@@ -128,31 +164,31 @@ class Camera : Fragment() {
         }, ContextCompat.getMainExecutor(context))
     }
 
-private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
-    context?.let { it1 ->
-        ContextCompat.checkSelfPermission(
-            it1, it
-        )
-    } == PackageManager.PERMISSION_GRANTED
-}
-
-private fun getOutputDirectory(): File {
-    val mediaDir = activity?.externalMediaDirs?.firstOrNull()?.let {
-        File(it, resources.getString(R.string.app_name)).apply { mkdirs() }
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        context?.let { it1 ->
+            ContextCompat.checkSelfPermission(
+                it1, it
+            )
+        } == PackageManager.PERMISSION_GRANTED
     }
-    return if (mediaDir != null && mediaDir.exists())
-        mediaDir else context?.filesDir!!
-}
 
-override fun onDestroy() {
-    super.onDestroy()
-    cameraExecutor.shutdown()
-}
+    private fun getOutputDirectory(): File {
+        val mediaDir = activity?.externalMediaDirs?.firstOrNull()?.let {
+            File(it, resources.getString(R.string.app_name)).apply { mkdirs() }
+        }
+        return if (mediaDir != null && mediaDir.exists())
+            mediaDir else context?.filesDir!!
+    }
 
-companion object {
-    private const val TAG = "CameraXBasic"
-    private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
-    private const val REQUEST_CODE_PERMISSIONS = 10
-    private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
-}
+    override fun onDestroy() {
+        super.onDestroy()
+        cameraExecutor.shutdown()
+    }
+
+    companion object {
+        private const val TAG = "CameraXBasic"
+        private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
+        private const val REQUEST_CODE_PERMISSIONS = 10
+        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+    }
 }
