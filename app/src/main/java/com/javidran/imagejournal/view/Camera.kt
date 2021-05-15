435935fe99b9bb34ac2a039635fa4f99b079de20
Joan <joan.gamez@est.fib.upc.edu>
2021-05-15 11:57:22 +0200

    camera fragment base code

--- app/src/main/java/com/javidran/imagejournal/view/Camera.kt
+++ app/src/main/java/com/javidran/imagejournal/view/Camera.kt
@@ -1,12 +1,25 @@
 package com.javidran.imagejournal.view
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.camera.core.*
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
 import com.javidran.imagejournal.R
 import com.javidran.imagejournal.databinding.FragmentCameraBinding
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_camera.*
+import java.io.File
+import java.util.*
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+
+typealias LumaListener = (luma: Double) -> Unit
 
 /**
  * A simple [Fragment] subclass.
@@ -14,11 +27,14 @@ import com.javidran.imagejournal.databinding.FragmentCameraBinding
  * create an instance of this fragment.
  */
 class Camera : Fragment() {
+    private var imageCapture: ImageCapture? = null
 
     private var _binding: FragmentCameraBinding? = null
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+    private lateinit var outputDirectory: File
+    private lateinit var cameraExecutor: ExecutorService
 
 
     override fun onDestroyView() {
@@ -34,6 +50,54 @@ class Camera : Fragment() {
         // Inflate the layout for this fragment
         _binding = FragmentCameraBinding.inflate(inflater, container, false)
         val view = binding.root
+
+        // Request camera permissions
+        if (allPermissionsGranted()) {
+            startCamera()
+        } else {
+            activity?.let {
+                ActivityCompat.requestPermissions(
+                    it, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
+            }
+        }
+
+        // Set up the listener for take photo button
+        camera_capture_button.setOnClickListener { takePhoto() }
+
+        outputDirectory = getOutputDirectory()
+
+        cameraExecutor = Executors.newSingleThreadExecutor()
+
         return view
+    }
+
+    private fun takePhoto() {}
+
+    private fun startCamera() {}
+
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        context?.let { it1 ->
+            ContextCompat.checkSelfPermission(
+                it1, it)
+        } == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun getOutputDirectory(): File {
+        val mediaDir = activity?.externalMediaDirs?.firstOrNull()?.let {
+            File(it, resources.getString(R.string.app_name)).apply { mkdirs() } }
+        return if (mediaDir != null && mediaDir.exists())
+            mediaDir else context?.filesDir!!
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        cameraExecutor.shutdown()
+    }
+
+    companion object {
+        private const val TAG = "CameraXBasic"
+        private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
+        private const val REQUEST_CODE_PERMISSIONS = 10
+        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
     }
 }
